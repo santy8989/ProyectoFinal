8b659d7eca431f0517184cd6ebc8a8c3fe103d14
santy8989 <santygarces@gmail.com>
2022-05-01 15:01:14 -0300

    firebase, y creacion de AuthService

--- Control-HorasAPP/src/environments/environment.ts
+++ Control-HorasAPP/src/environments/environment.ts
@@ -1,10 +1,20 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAOn3PzlPCqr25Vr-CSIggmAYITDmVLRpc",
+    authDomain: "control-horasapp.firebaseapp.com",
+    projectId: "control-horasapp",
+    storageBucket: "control-horasapp.appspot.com",
+    messagingSenderId: "640949412869",
+    appId: "1:640949412869:web:cc9872d71ad42a10dba86c",
+    measurementId: "G-CC63KFS3PH"
+  }
 };
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
