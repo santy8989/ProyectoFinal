85ebbb20a7430c9c73f4056de25690f7fe57a175
santy8989 <santygarces@gmail.com>
2022-05-01 15:20:44 -0300

    firebase a modulo y a servicio

--- Control-HorasAPP/src/app/app.module.ts
+++ Control-HorasAPP/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Control-HorasAPP/src/app/services/auth-service.service.ts
+++ Control-HorasAPP/src/app/services/auth-service.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
